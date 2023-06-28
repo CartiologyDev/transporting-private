472f82c07853ef0b45be3e74337624b7c877a263
Chris <70657828+BabalauCristian10C@users.noreply.github.com>
2023-06-29 02:57:05 +0300

    Small React Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
 import axios from "axios";
 import "./styles.css";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
 
-  getStatus() {
+  getStatus = () => {
     axios.get("/accountpage/workers").then((response) => {
+      console.log(response);
       if (response.status === 200) {
-        console.log(response.data)
-      } else {
-        console.log("Error")
+        console.log(1)
+        this.setState({ data: response.data });  // Set the response data in the component state
       }
+
     });
   }
 
   render() {
+    const { data } = this.state;
+
     return (
       <div>
         <button onClick={this.getStatus}>
           Click me!
         </button>
-        <ul className="users">
-            <p> статус    </p>
-        </ul>
+        {Object.keys(data).map(key => (
+          <div key={key}>
+            <span>{key}</span>: <span>{data[key]}</span>
+          </div>
+        ))}
+
       </div>
     );
   }
